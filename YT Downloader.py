5886202b9145ed7bc02bf3238de0dc2f092a3991
MuraliMK63 <117427754+MuraliMK63@users.noreply.github.com>
2024-06-06 12:10:32 +0530

    Update YT Downloader.py

--- YT Downloader.py
+++ YT Downloader.py
@@ -5,3 +5,6 @@ ytube=YouTube(link)
 yvideo=ytube.streams.get_highest_resolution()
 
 yvideo.download("D:\Beginner Projects\Youtube Downloader\Downloaded Videos")
+
+sk-proj-0EKYCUQmgrJ9EcLMYKBbT3BlbkFJKh297x6eeLwX9h0Uj3Rm
+sk-proj-NEjPxKTrLFbr4Zeum5qKT3BlbkFJaFQgU2o8YLtgdkfUH8Ak
